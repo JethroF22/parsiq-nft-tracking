7196bb6d60a558c3b9dc3e7ffdfdf532119a3225
Jethro Fredericks- <jethro.fredericks@protonmail.com>
2021-10-27 19:19:07 +0200

    initial commit

---
+++ apps/parsiq-nft-tracking/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'parsiq-nft-tracking',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/parsiq-nft-tracking',
+};
