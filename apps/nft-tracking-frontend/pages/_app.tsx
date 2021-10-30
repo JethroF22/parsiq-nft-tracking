41a7b14376ed89ee6ccfbf0ee5da8d9a36470ebd
Jethro Fredericks- <jethro.fredericks@protonmail.com>
2021-10-30 14:00:15 +0200

    [nft-tracking-frontend]: Create basic context setup

--- apps/nft-tracking-frontend/pages/_app.tsx
+++ apps/nft-tracking-frontend/pages/_app.tsx
@@ -1,11 +1,18 @@
 import { AppProps } from 'next/app';
+
 import Page from '../components/Page';
+
+import { Context } from '../context';
+
 import './styles.css';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const state: any = {};
   return (
     <Page>
-      <Component {...pageProps} />
+      <Context.Provider value={state}>
+        <Component {...pageProps} />
+      </Context.Provider>
     </Page>
   );
 }
