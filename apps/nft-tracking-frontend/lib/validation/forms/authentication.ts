8772ba18895e9af28b3b63718c10744925a6a89c
Jethro Fredericks- <jethro.fredericks@protonmail.com>
2021-10-30 18:10:16 +0200

    [nft-tracking-frontend]: Create validation schema for sign up form

---
+++ apps/nft-tracking-frontend/lib/validation/forms/authentication.ts
@@ -0,0 +1,32 @@
+import * as Yup from 'yup';
+
+import {
+  emailRegexPattern,
+  passwordRegexPattern,
+} from '@parsiq-nft-tracking/validation-library';
+
+export const loginFormValidationSchema = Yup.object().shape({
+  email: Yup.string()
+    .required('Please enter your email')
+    .matches(emailRegexPattern, 'Please enter a valid email address'),
+  password: Yup.string().required('Please enter your password'),
+});
+
+export const signUpFormValidationSchema = Yup.object().shape({
+  email: Yup.string()
+    .required('This field is required')
+    .matches(emailRegexPattern, 'Please enter a valid email address'),
+  password: Yup.string()
+    .required('This field is required')
+    .min(8, 'Password must be at least 8 characters')
+    .matches(
+      passwordRegexPattern,
+      'Your password needs at least one uppercase letter, one lowercase letter, 1 number and 1 special character'
+    ),
+  confirmPassword: Yup.string()
+    .transform((x) => (x === '' ? undefined : x))
+    .when('password', (password, schema) => {
+      if (password) return schema.required('Confirm Password is required');
+    })
+    .oneOf([Yup.ref('password')], 'Passwords must match'),
+});
