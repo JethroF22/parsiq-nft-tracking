15f37c1fd37170ad3ba4553c8ff1b1be402a54e3
Jethro Fredericks- <jethro.fredericks@protonmail.com>
2021-11-01 12:12:08 +0200

    [nft-tracking-frontend]: Create `emailVerificationHandler`

--- apps/nft-tracking-frontend/services/authentication.ts
+++ apps/nft-tracking-frontend/services/authentication.ts
@@ -1,6 +1,9 @@
 import { Auth } from 'aws-amplify';
+import { SignUpFormState } from '../lib/forms/authentication';
 
-export const signUpHandler = async (formState: any): Promise<any> => {
+export const signUpHandler = async (
+  formState: SignUpFormState
+): Promise<any> => {
   const { email, password } = formState;
   const { user } = await Auth.signUp({
     username: email,
@@ -8,4 +11,20 @@ export const signUpHandler = async (formState: any): Promise<any> => {
   });
 
   return user;
+};
+
+export const emailVerificationHandler = async (code: string, email: string) => {
+  try {
+    await Auth.confirmSignUp(email, code);
+    return '';
+  } catch (error) {
+    console.log('error', error);
+    if (error.code === 'CodeMismatchException') {
+      return 'Invalid code. Please enter a valid code or request a new code';
+    } else if (error.code === 'ExpiredCodeException') {
+      return 'Code has expired. Please request a new code';
+    } else {
+      return 'An unexpected server error occurred. Please try again later';
+    }
+  }
 };
