65bb467c2a97f406237941523fbd9cc1bb8c225e
Jethro Fredericks- <jethro.fredericks@protonmail.com>
2021-11-01 12:13:04 +0200

    [nft-tracking-frontend]: Add logic for verifying user email with code

--- apps/nft-tracking-frontend/components/authentication/EmailVerificationForm.tsx
+++ apps/nft-tracking-frontend/components/authentication/EmailVerificationForm.tsx
@@ -1,9 +1,15 @@
+import { useContext, useState } from 'react';
+import { useRouter } from 'next/router';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 
-import Button from './styled/Button';
 import { emailVerificationFormValidationSchema } from '../../lib/forms/authentication';
-import ModalContainer from '../common/ModalContainer';
+import { Context } from '../../context';
+import { emailVerificationHandler } from '../../services';
+
+import Button from './styled/Button';
+import Modal from '../../hocs/Modal';
+import LoadingIcon from '../common/LoadingIcon';
 
 interface EmailVerificationFormState {
   code: string;
@@ -14,16 +20,45 @@ export default function EmailVerificationForm() {
     resolver: yupResolver(emailVerificationFormValidationSchema),
   };
   const { register, handleSubmit } = useForm(formOptions);
+  const { state } = useContext(Context);
+  const [displayModal, setModalDisplayState] = useState(false);
+  const [authActionDispatched, setAuthActionState] = useState(false);
+  const router = useRouter();
 
-  const onSubmit = (formState: EmailVerificationFormState) => {
-    console.log('formState', formState);
+  const onSubmit = async (formState: EmailVerificationFormState) => {
+    try {
+      setAuthActionState(true);
+      const { username: email } = state.auth.user;
+      const result = await emailVerificationHandler(formState.code, email);
+      if (result === '') {
+        setModalDisplayState(true);
+        setAuthActionState(false);
+      }
+    } catch (error) {
+      console.log('error', error);
+      setAuthActionState(false);
+    }
   };
 
   return (
-    <ModalContainer>
-      <div className="title">Verify Your Email Address</div>
+    <>
+      {displayModal && (
+        <Modal>
+          <div className="modal-container">
+            <div className="modal-body">
+              <div className="modal-message">
+                You email address has been verified. You can now login using
+                your registered email address
+              </div>
+              <span className="modal-close" onClick={() => router.push('/')}>
+                &times;
+              </span>
+            </div>
+          </div>
+        </Modal>
+      )}
       <div className="body">
-        <div className="input-container">
+        <div className="input-group">
           <input
             type="text"
             placeholder="Verifcation Code"
@@ -32,11 +67,15 @@ export default function EmailVerificationForm() {
           />
         </div>
         <div className="input-group">
-          <Button onClick={handleSubmit(onSubmit)} className="button">
-            Confirm Email Address
-          </Button>
+          {authActionDispatched ? (
+            <LoadingIcon size="3x" />
+          ) : (
+            <Button onClick={handleSubmit(onSubmit)} className="button">
+              Confirm Email Address
+            </Button>
+          )}
         </div>
       </div>
-    </ModalContainer>
+    </>
   );
 }
