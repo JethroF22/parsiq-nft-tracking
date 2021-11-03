585f0b0867fe317c92ecc30a486cf5ffd3176201
Jethro Fredericks- <jethro.fredericks@protonmail.com>
2021-11-03 20:40:22 +0200

    [infrastructure/db]: Change DynamoDB schema

--- apps/infrastructure/configuration/db/main.tf
+++ apps/infrastructure/configuration/db/main.tf
@@ -15,8 +15,8 @@ provider "aws" {
 
 resource "aws_dynamodb_table" "api_dynamodb_table" {
   name         = "nft-tracking-table-${var.deployment_environment}"
-  hash_key     = "address"
-  range_key    = "user_id"
+  range_key    = "address"
+  hash_key     = "user_id"
   billing_mode = "PAY_PER_REQUEST"
 
   attribute {
