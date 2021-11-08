f446c53edceb872a0ba79ca7ccb5163f38e06687
Jethro Fredericks- <jethro.fredericks@protonmail.com>
2021-11-08 20:04:14 +0200

    [nft-tracking-frontend]: Create overlay for empty grid for addresses data grid

--- apps/nft-tracking-frontend/components/following/AddressListScreen.tsx
+++ apps/nft-tracking-frontend/components/following/AddressListScreen.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 import { makeStyles } from '@mui/styles';
 
+import useGetUserAddresses from '../../hooks/useGetUserAddresses';
 import useFetchUserAddresses from '../../hooks/useFetchUserAddresses';
 import { RequestState } from '../../types/http';
 
+import NoRowsOverlay from './NoRowsOverlay';
 import AddressListHeader from './AddressListHeader';
 import HomeScreen from '../common/HomeScreen';
 import LoadingIcon from '../common/LoadingIcon';
-import useGetUserAddresses from '../../hooks/useGetUserAddresses';
 import dataGridStyles from '../styles/dataGrid';
 
 const useStyles = makeStyles(dataGridStyles);
@@ -44,33 +45,21 @@ export default function AddressListScreen() {
   ];
   return (
     <HomeScreen>
-      {requestState === RequestState.RESOLVED && (
-        <>
-          <AddressListHeader />
-          <div style={{ height: '80vh', width: 800, margin: '3rem auto' }}>
-            <DataGrid
-              rows={rows}
-              columns={columns}
-              className={classes.root}
-              showColumnRightBorder={false}
-            />
-          </div>
-        </>
-      )}
-      {requestState === RequestState.LOADING && (
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '80vh',
-            width: 800,
-            margin: '3rem auto',
-          }}
-        >
-          <LoadingIcon size="5x" />
+      <>
+        <AddressListHeader />
+        <div style={{ height: '80vh', width: 800, margin: '3rem auto' }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            className={classes.root}
+            showColumnRightBorder={false}
+            components={{
+              NoRowsOverlay,
+            }}
+            loading={requestState === RequestState.LOADING}
+          />
         </div>
-      )}
+      </>
     </HomeScreen>
   );
 }
---
+++ apps/nft-tracking-frontend/components/following/NoRowsOverlay.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { GridOverlay } from '@mui/x-data-grid';
+import { createTheme } from '@mui/material/styles';
+import { createStyles, makeStyles } from '@mui/styles';
+
+const defaultTheme = createTheme();
+const useStyles = makeStyles(
+  (theme) =>
+    createStyles({
+      root: {
+        flexDirection: 'column',
+        '& .ant-empty-img-1': {
+          fill: theme.palette.mode === 'light' ? '#aeb8c2' : '#262626',
+        },
+        '& .ant-empty-img-2': {
+          fill: theme.palette.mode === 'light' ? '#f5f5f7' : '#595959',
+        },
+        '& .ant-empty-img-3': {
+          fill: theme.palette.mode === 'light' ? '#dce0e6' : '#434343',
+        },
+        '& .ant-empty-img-4': {
+          fill: theme.palette.mode === 'light' ? '#fff' : '#1c1c1c',
+        },
+        '& .ant-empty-img-5': {
+          fillOpacity: theme.palette.mode === 'light' ? '0.8' : '0.08',
+          fill: theme.palette.mode === 'light' ? '#f5f5f5' : '#fff',
+        },
+      },
+      label: {
+        marginTop: theme.spacing(1),
+        fontSize: 16,
+      },
+      // link: {
+      //   fontSize: 16,
+      //   textDecoration: 'underline',
+      //   color: '#629ecb',
+      //   cursor: 'pointer',
+      //   '&:hover': {
+      //     color: '#95d1fe',
+      //   },
+      // },
+    }),
+  { defaultTheme }
+);
+
+function NoRowsOverlay({ message }) {
+  const classes = useStyles();
+  return (
+    <GridOverlay className={classes.root}>
+      <div className={classes.label}>No addresses tracked.</div>
+    </GridOverlay>
+  );
+}
+
+export default NoRowsOverlay;
